18a9a7bcf6e8a4d1912a3e445c1f87dd72001c9e
Rachel Spears <103690982+Rosepetal2022@users.noreply.github.com>
2025-04-24 02:18:31 +0000

    added basic login card

--- frontend/member_and_points_manager/src/pages/Login.js
+++ frontend/member_and_points_manager/src/pages/Login.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Card, CardLink, CardBody, CardTitle, FormGroup, Label, Input } from 'reactstrap';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login works!</h1>
-        </div>
+
+        <Card id="login-card">
+            <CardBody>
+                <CardTitle tag="h5">
+                    Login
+                </CardTitle>
+            </CardBody>
+            <CardBody>
+                <FormGroup>
+                    <Label for="password">
+                        Email
+                    </Label>
+                    <Input
+                        id="email"
+                        name="email"
+                        placeholder="email"
+                        type="email"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="password">
+                        Password
+                    </Label>
+                    <Input
+                        id="password"
+                        name="password"
+                        placeholder="password"
+                        type="password"
+                    />
+                </FormGroup>
+            </CardBody>
+            <CardLink>
+                Don't have an account? create one here
+            </CardLink>
+        </Card>
     );
 };
 
