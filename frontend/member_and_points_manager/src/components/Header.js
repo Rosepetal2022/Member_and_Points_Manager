301dd205d9374ec40c63ab12b58dc0ac3e2bc1e8
Rachel Spears <103690982+Rosepetal2022@users.noreply.github.com>
2025-04-24 01:55:11 +0000

    basic header/footer

--- frontend/member_and_points_manager/src/components/Header.js
+++ frontend/member_and_points_manager/src/components/Header.js
@@ -1,38 +1,37 @@
 
 import React from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Header = () => {
     return (
-        <>
-            <Navbar
-                className="my-2"
-                color="dark"
-                dark
-            >
+        <Navbar className="my-2" color="dark" dark expand="md">
                 <NavbarBrand href="/">
-                    <img
+                LOGO Goes Here
+                    {/* <img
                         alt="logo"
                         src="/logo-white.svg"
                         style={{
                             height: 40,
                             width: 40
                         }}
-                    />
+                    /> */}
                 </NavbarBrand>
-                <NavItem>
-                    <NavLink href="/Login">Login</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/AdminHome">Admin</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/MemberHome">Members</NavLink>
-                </NavItem>
-
-            </Navbar>
-        </>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/Login">Login</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/AdminHome">Admin</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/MemberHome">Members</NavLink>
+                    </NavItem>
+                </Nav>
+        </Navbar>
     );
 };
 
 export default Header;
+
