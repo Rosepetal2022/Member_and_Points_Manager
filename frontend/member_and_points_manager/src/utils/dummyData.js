a10e2fc833d49cc2179f90dd3523a7f5115fc257
Rachel Spears <103690982+Rosepetal2022@users.noreply.github.com>
2025-04-30 02:33:34 +0000

    started on the points page

---
+++ frontend/member_and_points_manager/src/utils/dummyData.js
@@ -0,0 +1,167 @@
+const horseShows = [
+    {
+      showName: 'Cerafina',
+      OwnerName: ['Tom Smith'],
+      Riders: ['Tom Smith'],
+      jumperPoints: [],
+      equitationPoints: [
+        { division: 'Childrens Equitation', pointsEarned: 10 }
+      ],
+      hunterPoints: [
+        { division: 'short Stirrup Hunter', pointsEarned: 5 }
+      ]
+    },
+    {
+      showName: 'Starlight',
+      OwnerName: ['John Doe'],
+      Riders: ['John Doe', 'Jane Smith'],
+      jumperPoints: [
+        { division: '.80m Jumper', pointsEarned: 40 }
+      ],
+      equitationPoints: [],
+      hunterPoints: [
+        { division: 'Short Stirrup Hunter', pointsEarned: 10 }
+      ]
+    },
+    {
+      showName: 'Moonlight',
+      OwnerName: ['Alice Johnson'],
+      Riders: ['Alice Johnson', 'Bob Brown'],
+      jumperPoints: [
+        { division: '1.30m Jumper', pointsEarned: 50 },
+      ],
+      equitationPoints: [
+        { division: 'Junior Equitation', pointsEarned: 20 }
+      ],
+      hunterPoints: []
+    },
+    {
+      showName: 'Sunshine',
+      OwnerName: ['Emily Clark'],
+      Riders: ['Emily Clark', 'David Lee'],
+      jumperPoints: [],
+      equitationPoints: [
+        { division: 'Adults Equitation', pointsEarned: 10 }
+      ],
+      hunterPoints: [
+        { division: 'Low Adult Hunter', pointsEarned: 20 }
+      ]
+    },
+    {
+      showName: 'Twilight',
+      OwnerName: ['Michael Smith'],
+      Riders: ['Michael Smith', 'Sarah White'],
+      jumperPoints: [
+        { division: '.70m Jumper', pointsEarned: 15 }
+      ],
+      equitationPoints: [
+        { division: 'Adult Equitation', pointsEarned: 15 }
+      ],
+      hunterPoints: [
+        { division: 'Adult Hunter', pointsEarned: 25 }
+      ]
+    },
+    {
+      showName: 'Aurora',
+      OwnerName: ['Laura Green'],
+      Riders: ['Laura Green', 'Chris Black'],
+      jumperPoints: [
+        { division: '1.0m Jumper', pointsEarned: 80 },
+        { division: '1.10m Jumper', pointsEarned: 30 }
+      ],
+      equitationPoints: [
+        { division: 'Adult Equitation', pointsEarned: 20 }
+      ],
+      hunterPoints: [
+        { division: 'Adults Hunter', pointsEarned: 30 }
+      ]
+    },
+    {
+      showName: 'Galaxy',
+      OwnerName: ['Nancy Blue'],
+      Riders: ['Nancy Blue', 'Tom Red'],
+      jumperPoints: [
+        { division: '.80m Jumper', pointsEarned: 90 },
+        { division: '1.0m Jumper', pointsEarned: 25 }
+      ],
+      equitationPoints: [
+        { division: 'Childrens Equitation', pointsEarned: 10 }
+      ],
+      hunterPoints: [
+        { division: 'Junior Hunter', pointsEarned: 35 }
+      ]
+    },
+    {
+      showName: 'Comet',
+      OwnerName: ['Oliver Yellow'],
+      Riders: ['Oliver Yellow', 'Sophia Pink'],
+      jumperPoints: [
+        { division: '.90m Jumper', pointsEarned: 20 }
+      ],
+      equitationPoints: [
+        { division: 'Adult Equitation', pointsEarned: 15 }
+      ],
+      hunterPoints: [
+        { division: 'Amateur Owner Hunter', pointsEarned: 40 }
+      ]
+    },
+    {
+      showName: 'Meteor',
+      OwnerName: ['Peter Purple'],
+      Riders: ['Peter Purple', 'Linda Orange'],
+      jumperPoints: [
+        { division: '.90m Jumper', pointsEarned: 110 },
+        { division: '.80m Jumper', pointsEarned: 15 }
+      ],
+      equitationPoints: [
+        { division: 'Junior Equitation', pointsEarned: 20 }
+      ],
+      hunterPoints: [
+        { division: 'Low Childrens Hunter', pointsEarned: 15 }
+      ]
+    },
+    {
+      showName: 'Nebula',
+      OwnerName: ['Quinn Gray'],
+      Riders: ['Quinn Gray', 'Rebecca Brown'],
+      jumperPoints: [
+        { division: '1.20m Jumper', pointsEarned: 120 },
+        { division: '1.10m Jumper', pointsEarned: 30 }
+      ],
+      equitationPoints: [
+        { division: 'Childrens Equitation', pointsEarned: 10 }
+      ],
+      hunterPoints: [
+        { division: 'Low Childrens Hunter', pointsEarned: 20 }
+      ]
+    },
+    {
+      showName: 'Starburst',
+      OwnerName: ['Sam Green'],
+      Riders: ['Sam Green', 'Tina Blue'],
+      jumperPoints: [
+        { division: '1.20m Jumper', pointsEarned: 130 },
+        { division: '1.0m Jumper', pointsEarned: 25 }
+      ],
+      equitationPoints: [
+        { division: 'Adult Equitation', pointsEarned: 15 }
+      ],
+      hunterPoints: [
+        { division: 'Childrens Hunter', pointsEarned: 25 }
+      ]
+    },
+    {
+      showName: 'Supernova',
+      OwnerName: ['Uma Red'],
+      Riders: ['Uma Red', 'Victor Yellow'],
+      jumperPoints: [
+        { division: '1.30m Jumper', pointsEarned: 140 },
+        { division: '.90m Jumper', pointsEarned: 20 }
+      ],
+      equitationPoints: [
+        { division: 'Junior Equitation', pointsEarned: 20 }
+      ],
+      hunterPoints: [
+        { division: 'Children Hunter', pointsEarned: 30 }
+      ]
+    }]
