3d4fe8234c5d5fa743ad514792ae7e6fd293ae8b
brandonO91 <brandon.oyama.0526@gmail.com>
2025-06-03 20:51:26 -0700

    Updated navigation and urls to api calls

--- frontend/member_and_points_manager/src/api/classesApi.js
+++ frontend/member_and_points_manager/src/api/classesApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL || 'http://localhost:8888/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8888/api';
 
 
 export const getAllClasses = () => axios.get(`${API_URL}/classes`);
